b97f236b9005d91166ea8f4569fb159ef2ab2bf1
abien15 <ajb501@scarletmail.rutgers.edu>
2025-05-02 12:36:55 -0400

    Update image.h

--- image.h
+++ image.h
@@ -48,3 +48,108 @@ public:
 };
 
 #endif // IMAGE_H
+
+
+
+
+
+
+
+
+
+//Andrew's version of image
+
+#ifndef IMAGEHEADER_H
+#define IMAGEHEADER_H
+
+#include <iostream>
+#include "matrixheader.h"
+
+template <typename T>
+class Image : public Matrix<T> {
+public:
+    // Inherit constructors
+    using Matrix<T>::Matrix;
+
+    // Scale the image by a scalar
+    void scale(T scalar) {
+        for (int i = 0; i < this->numRows(); ++i) {
+            for (int j = 0; j < this->numCols(); ++j) {
+                this->at(i, j) *= scalar;
+            }
+        }
+    }
+
+    // Element-wise addition
+    Image<T> operator+(const Image<T>& other) const {
+        Matrix<T> result = Matrix<T>::operator+(other);
+        return static_cast<Image<T>>(result);
+    }
+
+    // Element-wise subtraction
+    Image<T> operator-(const Image<T>& other) const {
+        Matrix<T> result = Matrix<T>::operator-(other);
+        return static_cast<Image<T>>(result);
+    }
+
+    // Scalar multiplication
+    Image<T> operator*(T scalar) const {
+        Matrix<T> result = Matrix<T>::operator*(scalar);
+        return static_cast<Image<T>>(result);
+    }
+
+    // Element-wise multiplication
+    Image<T> multiply(const Image<T>& other) const {
+        if (this->numRows() != other.numRows() || this->numCols() != other.numCols()) {
+            std::cerr << "Error: Image dimensions do not match for element-wise multiplication." << std::endl;
+            exit(1);
+        }
+
+        Image<T> result(this->numRows(), this->numCols());
+        for (int i = 0; i < this->numRows(); ++i) {
+            for (int j = 0; j < this->numCols(); ++j) {
+                result.at(i, j) = this->at(i, j) * other.at(i, j);
+            }
+        }
+        return result;
+    }
+
+    void resize(int newRows, int newCols) {
+        if constexpr (!std::is_same<T, unsigned char>::value) {
+            std::cerr << "Resize only supports unsigned char pixel types.\n";
+            return;
+        }
+    
+        int oldRows = this->numRows();
+        int oldCols = this->numCols();
+    
+        // Flatten original data
+        std::vector<unsigned char> input(oldRows * oldCols);
+        for (int i = 0; i < oldRows; ++i)
+            for (int j = 0; j < oldCols; ++j)
+                input[i * oldCols + j] = this->at(i, j);
+    
+        // Prepare output buffer
+        std::vector<unsigned char> output(newRows * newCols);
+    
+        // Resize using stb
+        int success = stbir_resize_uint8(
+            input.data(), oldCols, oldRows, 0,
+            output.data(), newCols, newRows, 0, 1
+        );
+    
+        if (!success) {
+            std::cerr << "Resize failed.\n";
+            return;
+        }
+    
+        // Replace internal data
+        *this = Image<T>(newRows, newCols);
+        for (int i = 0; i < newRows; ++i)
+            for (int j = 0; j < newCols; ++j)
+                this->at(i, j) = output[i * newCols + j];
+    }
+    
+};
+
+#endif // IMAGEHEADER_H
