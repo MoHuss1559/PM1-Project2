47113206dc42114cd9b8cd90e5b5e0fe18c710cc
MoHuss1559 <mh1559@scarletmail.rutgers.edu>
2025-05-02 12:50:36 -0400

    Update vector.h

--- vector.h
+++ vector.h
@@ -2,80 +2,77 @@
 #define VECTOR_H
 
 #include <iostream>
-#include <cassert>
+#include <stdexcept> // For std::out_of_range
 
 template <typename T>
 class Vector {
 private:
-    int length;
+    size_t length;
     T* data;
+
 public:
     // Constructor
-    Vector(int size) : length(size), data(new T[size]) {}
-
-    // Copy constructor
-    Vector(const Vector<T>& other) : length(other.length), data(new T[other.length]) {
-        for (int i = 0; i < length; ++i) {
-            data[i] = other.data[i];
-        }
-    }
-
-    // Copy assignment operator
-    Vector<T>& operator=(const Vector<T>& other) {
-        if (this != &other) {
-            delete[] data;
-            length = other.length;
-            data = new T[length];
-            for (int i = 0; i < length; ++i) {
-                data[i] = other.data[i];
-            }
-        }
-        return *this;
-    }
+    explicit Vector(size_t size = 0) : length(size), data(new T[size]()) {}
 
     // Destructor
     ~Vector() { delete[] data; }
 
-    // Accessors
-    T& operator[](int index) {
-        assert(index >= 0 && index < length);
-        return data[index];
+    // Copy Constructor
+    Vector(const Vector& other) : length(other.length), data(new T[other.length]) {
+        for (size_t i = 0; i < length; ++i)
+            data[i] = other.data[i];
     }
 
-    const T& operator[](int index) const {
-        assert(index >= 0 && index < length);
-        return data[index];
-    }
-
-    T& at(int index) {
-        assert(index >= 0 && index < length);
-        return data[index];
-    }
-
-    const T& at(int index) const {
-        assert(index >= 0 && index < length);
-        return data[index];
-    }
-
-    int size() const { return length; }
-
-    // Addition
-    Vector<T> operator+(const Vector<T>& other) const {
-        assert(length == other.length);
-        Vector<T> result(length);
-        for (int i = 0; i < length; ++i) {
-            result[i] = data[i] + other[i];
+    // Copy Assignment
+    Vector& operator=(const Vector& other) {
+        if (this != &other) {
+            delete[] data;
+            length = other.length;
+            data = new T[length];
+            for (size_t i = 0; i < length; ++i)
+                data[i] = other.data[i];
         }
+        return *this;
+    }
+
+    // Accessors (with bounds checking)
+    T& operator[](size_t index) {
+        if (index >= length) throw std::out_of_range("Vector index out of range!");
+        return data[index];
+    }
+
+    const T& operator[](size_t index) const {
+        if (index >= length) throw std::out_of_range("Vector index out of range!");
+        return data[index];
+    }
+
+    // Size query
+    size_t size() const { return length; }
+
+    // Arithmetic operations
+    Vector operator+(const Vector& other) const {
+        if (length != other.length)
+            throw std::invalid_argument("Vector sizes must match for addition!");
+        Vector result(length);
+        for (size_t i = 0; i < length; ++i)
+            result[i] = data[i] + other[i];
         return result;
     }
 
-    // Subtraction
-    Vector<T> operator-(const Vector<T>& other) const {
-        assert(length == other.length);
-        Vector<T> result(length);
-        for (int i = 0; i < length; ++i) {
+    Vector operator-(const Vector& other) const {
+        if (length != other.length)
+            throw std::invalid_argument("Vector sizes must match for subtraction!");
+        Vector result(length);
+        for (size_t i = 0; i < length; ++i)
             result[i] = data[i] - other[i];
-        }
+        return result;
+    }
+
+    // Scalar multiplication
+    Vector operator*(T scalar) const {
+        Vector result(length);
+        for (size_t i = 0; i < length; ++i)
+            result[i] = data[i] * scalar;
         return result;
     }
 };
